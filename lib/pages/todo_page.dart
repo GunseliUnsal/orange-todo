bac094ecc88137b62e953d0cce297b94b7fa3207
Günseli Ünsal <gunseliunsal@hotmail.com>
2024-01-15 23:38:06 +0300

    add splash screen

--- lib/pages/todo_page.dart
+++ lib/pages/todo_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:orange_todo/constants/colors.dart';
 
 class TodoPage extends StatefulWidget {
   const TodoPage({super.key});
@@ -10,6 +11,22 @@ class TodoPage extends StatefulWidget {
 class _TodoPageState extends State<TodoPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.only(right: 58.0),
+          child: Center(
+            child: Image.asset(
+              "assets/png/orange_image.png",
+              width: 40,
+            ),
+          ),
+        ),
+        backgroundColor: navbarColor,
+      ),
+      body: const Column(
+        children: [],
+      ),
+    );
   }
 }
