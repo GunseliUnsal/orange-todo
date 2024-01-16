84db0611d2f93caa2620122a267146c0bc64a533
Günseli Ünsal <gunseliunsal@hotmail.com>
2024-01-16 12:04:46 +0300

    updated

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -11,6 +11,11 @@ class StartPage extends StatefulWidget {
   State<StartPage> createState() => _StartPageState();
 }
 
+const String _orangeImg = "assets/png/orange_image.png";
+const String _title = "ORGANIZE YOUR DAY";
+const String _description = "add your daily to-do list";
+const String _buttonText = "Start to Plan";
+
 class _StartPageState extends State<StartPage> {
   @override
   Widget build(BuildContext context) {
@@ -19,44 +24,66 @@ class _StartPageState extends State<StartPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset("assets/png/orange_image.png", width: 220),
+            Image.asset(_orangeImg, width: 220),
             const SizedBox(height: 10), // Adjust the height between image and text
-            Padding(
-              padding: const EdgeInsets.only(bottom: 40),
-              child: Column(
-                children: [
-                  Text(
-                    "ORGANIZE YOUR DAY",
-                    style: TextStyle(color: orangeTextColor, fontSize: 28, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    "add your daily to-do list",
-                    style: TextStyle(color: greenTextColor, fontSize: 18),
-                  ),
-                ],
-              ),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 40),
+              child: _StartText(),
             ),
             const SizedBox(height: 60), // Adjust the height between text and button
-            GestureDetector(
-              onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const TodoPage()));
-              },
-              child: Container(
-                height: 60,
-                width: 190,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.orange,
-                ),
-                child: const Center(
-                  child: Text(
-                    "Start to Plan",
-                    style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20, color: Colors.white),
-                  ),
-                ),
-              ),
-            ),
+            const _Button(),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class _StartText extends StatelessWidget {
+  const _StartText({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          _title,
+          style: TextStyle(color: orangeTextColor, fontSize: 28, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          _description,
+          style: TextStyle(color: greenTextColor, fontSize: 18),
+        ),
+      ],
+    );
+  }
+}
+
+class _Button extends StatelessWidget {
+  const _Button({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => const TodoPage()));
+      },
+      child: Container(
+        height: 60,
+        width: 190,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: buttonColor,
+        ),
+        child: const Center(
+          child: Text(
+            _buttonText,
+            style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20, color: Colors.white),
+          ),
         ),
       ),
     );
--- lib/pages/todo_page.dart
+++ lib/pages/todo_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:orange_todo/constants/colors.dart';
+import 'package:orange_todo/util/dialog_box.dart';
+import 'package:orange_todo/util/todo_tile.dart';
 
 class TodoPage extends StatefulWidget {
   const TodoPage({super.key});
@@ -8,25 +10,85 @@ class TodoPage extends StatefulWidget {
   State<TodoPage> createState() => _TodoPageState();
 }
 
+const String _image = "assets/png/orange_image.png";
+
 class _TodoPageState extends State<TodoPage> {
+  //* text controller
+  final _controller = TextEditingController();
+
+  //* list of todo tasks
+  List toDoList = [
+    ["Make reels video", true],
+    ["Go out with your sis", false]
+  ];
+
+  //* checkbox was tapped
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+  //*save new task
+  void saveNewTask() {
+    setState(() {
+      toDoList.add([_controller.text, false]);
+      _controller.clear();
+    });
+    Navigator.of(context).pop();
+  }
+
+  //* create a new task
+  void createNewTask() {
+    showDialog(
+        context: context,
+        builder: (builder) {
+          return DialogBox(
+            controller: _controller,
+            onSave: saveNewTask,
+            onCancel: () => Navigator.of(context).pop(),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Padding(
-          padding: const EdgeInsets.only(right: 58.0),
-          child: Center(
-            child: Image.asset(
-              "assets/png/orange_image.png",
-              width: 40,
-            ),
+        appBar: _appBar(),
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.only(bottom: 18.0),
+          child: FloatingActionButton(
+            onPressed: createNewTask,
+            backgroundColor: buttonColor,
+            shape: const CircleBorder(),
+            elevation: 3,
+            child: const Icon(Icons.add),
           ),
         ),
-        backgroundColor: navbarColor,
+        body: ListView.builder(
+          itemCount: toDoList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ToDoTile(
+                taskName: toDoList[index][0],
+                taskCompleted: toDoList[index][1],
+                onChanged: (value) => checkBoxChanged(value, index));
+          },
+        ));
+  }
+
+  AppBar _appBar() {
+    return AppBar(
+      elevation: 0,
+      title: Padding(
+        padding: const EdgeInsets.only(right: 58.0),
+        child: Center(
+          child: Image.asset(
+            _image,
+            width: 40,
+          ),
+        ),
       ),
-      body: const Column(
-        children: [],
-      ),
+      backgroundColor: navbarColor,
     );
   }
 }
